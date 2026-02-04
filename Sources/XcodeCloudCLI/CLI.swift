fd5b3eddbae12052106edda2aefc159eb914e0c8
Emory Al-Imam <emorya@gmail.com>
2026-02-03 16:11:40 -0800

    Bump version to 1.0.5

--- Sources/XcodeCloudCLI/CLI.swift
+++ Sources/XcodeCloudCLI/CLI.swift
@@ -38,7 +38,7 @@ public struct XcodeCloud: ParsableCommand {
               Get build status with table output:
                 $ xcodecloud builds get <build-id> -o table
             """,
-        version: "1.0.4",
+        version: "1.0.5",
         subcommands: [
             InteractiveCommand.self,
             AuthCommand.self,
